041b3cf86bd90ee98b1c9801b4c14401617632da
Frenit <nkumar301402@gmail.com>
2022-12-05 07:58:17 +0530

    -w-

--- cogs/actions.py
+++ cogs/actions.py
@@ -1,7 +1,7 @@
 import discord
 import random
-import giphy_client
-from giphy_client.rest import ApiException
+import aiohttp
+import asyncio
 from discord import app_commands
 from discord.ext import commands
 
@@ -19,61 +19,34 @@ class Action(commands.Cog):
     @commands.command()
     async def hug(self, ctx, user: discord.User = None, *, Notes=None):
         
-        api_key="EFYlbleRxNekO5wTwwxmLxLf7y5NAQ9t"
-        api_instance = giphy_client.DefaultApi()
-        
-        api_response=api_instance.gifs_search_get(api_key,q="anime hug",limit=30,rating='g')
-        hugGifs = list(api_response.data)
-        giff=random.choice(hugGifs)
-        ur=giff.images.original.url
+        kawaii_token ="288311787072389120.wddZbVkXYndQaUNlFo9H"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f'https://kawaii.red/api/gif/hug/token={kawaii_token}') as r:
+                js = await r.json()
+                ur=str(js["response"])
 
         if not user:
-            em = discord.Embed(description=f"{ctx.message.author.mention} aww, are you that alone. here a hugs just for you", color = 0x000000)
+            em = discord.Embed(description=f"{ctx.author.mention} aww you are that alone... here a hug for you ", color = 0x000000)
             em.set_image(url=f"{ur}")
+
             if Notes is None:
                 await ctx.send(embed=em)
             return
 
-        embed = discord.Embed(description=f"{ctx.message.author.mention} hugs {user.mention}", color = 0x000000)
+        embed = discord.Embed(description=f"{ctx.author.mention} hugs  {user.mention}", color = 0x000000)
         embed.set_image(url=f"{ur}")
-
+        
         if Notes is None:
             await ctx.send(embed=embed)
         else:
-            embedZ = discord.Embed(description=f"{ctx.message.author.mention} hugs {user.mention}, {Notes}", color = 0x000000)
+            embedZ = discord.Embed(description=f"{ctx.author.mention} hugs  {user.mention}, {Notes}", color = 0x000000)
             embedZ.set_image(url=f"{ur}")
             await ctx.send(embed=embedZ)
 
 
 #Kiss
 
-    @commands.command()
-    async def kiss(self, ctx, user: discord.User = None, *, Notes=None):
         
-        api_key="EFYlbleRxNekO5wTwwxmLxLf7y5NAQ9t"
-        api_instance = giphy_client.DefaultApi()
-        
-        api_response=api_instance.gifs_search_get(api_key,q="anime kiss",limit=30,rating='g')
-        kissGifs = list(api_response.data)
-        giff=random.choice(kissGifs)
-        ur=giff.images.original.url
-
-        if not user:
-            em = discord.Embed(description=f"{ctx.message.author.mention} aww, are you that alone. here a kiss just for you", color = 0x000000)
-            em.set_image(url=f"{ur}")
-            if Notes is None:
-                await ctx.send(embed=em)
-            return
-
-        embed = discord.Embed(description=f"{ctx.message.author.mention} kisses {user.mention}", color = 0x000000)
-        embed.set_image(url=f"{ur}")
-
-        if Notes is None:
-            await ctx.send(embed=embed)
-        else:
-            embedZ = discord.Embed(description=f"{ctx.message.author.mention} kisses {user.mention}, {Notes}", color = 0x000000)
-            embedZ.set_image(url=f"{ur}")
-            await ctx.send(embed=embedZ)
 
 
 #Cuddle
